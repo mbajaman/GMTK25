26d039d557f5387d9d333cba396bcb3231680849
unknown <charleshuang97@gmail.com>
2025-08-02 23:07:50 -0700

    added main menu

---
+++ Assets/Source/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Clock Blockout");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit(); // Won't work in editor
+    }
+}
