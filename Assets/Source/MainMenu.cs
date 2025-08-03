480475d20b9fb8cfd556aa28be432f4efc255136
unknown <charleshuang97@gmail.com>
2025-08-03 09:20:22 -0700

    cleaned up folders

--- Assets/Source/MainMenu.cs
+++ Assets/Source/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
 
     public void MainMenuBtn()
     {
-        SceneManager.LoadScene("AnotherMainMenu");
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Quit()
